21dcaf352e28bbcb9fd445e7ee691153a732fc89
itslu666 <philipp.j6y@gmail.com>
2024-11-05 00:26:07 +0100

    add args

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+use clap::{Arg, Command};
+
+fn dig() {
+    println!("dig")
+}
+
+fn resurrect() {
+    println!("resurrect")
+}
+
 fn main() {
-    println!("Hello, world!");
+    let matches = Command::new("ripper")
+    .version("0.1")
+    .about("A CLI tool to manage Linux trash bin")
+    .arg(Arg::new("dig")
+        .short('d')
+        .long("dig")
+        .help("Dig out (List) all trashed items"))
+
+    .arg(Arg::new("resurrect")
+        .short('r')
+        .long("resurrect")
+        .help("Resurrect (Recover) item from trash bin"))
+
+    .get_matches();
+
+    if matches.contains_id("dig") {
+        dig();
+    }
+
+    if matches.contains_id("resurrect") {
+        resurrect();
+    }
 }
