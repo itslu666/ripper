0decd68bc8f7b6211b140715e752f0ddbd2f8282
itslu666 <philipp.j6y@gmail.com>
2024-11-05 12:34:43 +0100

    added function if no flag is set

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,50 @@
 use clap::{Arg, Command};
 
 fn dig() {
-    println!("dig")
+    println!("dig");
 }
 
 fn resurrect() {
-    println!("resurrect")
+    println!("resurrect");
+}
+
+fn delete_file() {
+    println!("delete files");
 }
 
 fn main() {
     let matches = Command::new("ripper")
-    .version("0.1")
-    .about("A CLI tool to manage Linux trash bin")
-    .arg(Arg::new("dig")
-        .short('d')
-        .long("dig")
-        .help("Dig out (List) all trashed items"))
+        .version("0.1")
+        .about("A CLI tool to manage Linux trash bin")
+        .arg(
+            Arg::new("dig")
+                .short('d')
+                .long("dig")
+                .help("Dig out (List) all trashed items")
+                .action(clap::ArgAction::SetTrue),
+        )
+        .arg(
+            Arg::new("resurrect")
+                .short('r')
+                .long("resurrect")
+                .help("Resurrect (Recover) item from trash bin")
+                .action(clap::ArgAction::SetTrue),
+        )
+        .get_matches();
 
-    .arg(Arg::new("resurrect")
-        .short('r')
-        .long("resurrect")
-        .help("Resurrect (Recover) item from trash bin"))
+    // delete files if no flags are set
+    if !matches.get_flag("dig") && !matches.get_flag("resurrect") {
+        delete_file();
+        return; // End function after files deleted
+    }
 
-    .get_matches();
-
-    if matches.contains_id("dig") {
+    // check if dig flag is set
+    if matches.get_flag("dig") {
         dig();
     }
 
-    if matches.contains_id("resurrect") {
+    // check if resurrect flag is set
+    if matches.get_flag("resurrect") {
         resurrect();
     }
 }
